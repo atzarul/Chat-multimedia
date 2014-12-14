573da3e2c8191b0751b3459d2dd9f7efa0869bb5
oanatodea <oana4_ct@yahoo.com>
2014-12-14 18:41:06 +0200

    Added Friend class

---
+++ CM - Server/src/com/is/chatmultimedia/client/models/Friend.java
@@ -0,0 +1,22 @@
+package com.is.chatmultimedia.client.models;
+
+public class Friend {
+  
+  private String name;
+  private String username;
+  
+  public Friend(String name, String username){
+    this.name = name;
+    this.username = username;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+  
+  
+}
